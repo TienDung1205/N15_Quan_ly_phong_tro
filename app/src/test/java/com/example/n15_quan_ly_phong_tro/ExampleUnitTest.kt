705368ee6cd43b0dc17198bbe3ff1c82ace0b002
Bùi Tiến Dũng <buitiendung12517@gmail.com>
2026-03-20 14:35:28 +0700

    first commit

---
+++ app/src/test/java/com/example/n15_quan_ly_phong_tro/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.n15_quan_ly_phong_tro
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "N15_Quan_ly_phong_tro"
+include(":app")
+ 
